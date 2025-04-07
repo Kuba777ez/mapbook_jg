07c51f2d65afeebcc066d3aeb367e732da363534
Kuba777ez <kubaa.gil@o2.pl>
2025-04-07 10:14:53 +0200

    chore: code structure upadate

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
-users:list[dict] = [
-    {'name': 'Kuba', 'location': 'Szczecin', 'posts': 500, },
-    {'name': 'Julka', 'location': 'Płońsk', 'posts': 100, },
-    {'name': 'Marcin', 'location': 'Poznań', 'posts': 420, }
-]
+from utils.controller import get_user_info
+from utils.model import users
 
 
-def get_user_info(users_data: list[dict])->None:
-    for user in users_data:
-        print(f'Twój znajomy: {user["name"]} z {user["location"]} opublikował {user["posts"]}')
+def main():
+    get_user_info(users)
 
-get_user_info(users)
+if __name__ == '__main__':
+    main()
---
+++ utils/model.py
@@ -0,0 +1,5 @@
+users: list[dict] = [
+    {'name': 'Kuba', 'location': 'Szczecin', 'posts': 500, },
+    {'name': 'Julka', 'location': 'Płońsk', 'posts': 100, },
+    {'name': 'Marcin', 'location': 'Poznań', 'posts': 420, }
+]
