b6fe303138b8b57312a4d9899ffa64376e5fe2b9
Kuba777ez <kubaa.gil@o2.pl>
2025-04-07 11:14:50 +0200

    wip: new menu option

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from utils.controller import get_user_info
+from utils.controller import get_user_info, add_user
 from utils.model import users
+
 
 
 def main():
@@ -7,12 +8,14 @@ def main():
         print('==========MENU============')
         print('0 - Exit')
         print('1 - Get user info')
+        print('2 - Add user')
         print('=========================')
 
 
         choice=input('Enter your choice:')
         if choice=='0': break
         if choice=='1': get_user_info(users)
+        if choice=='2': add_user(users)
 
 if __name__ == '__main__':
     main()
