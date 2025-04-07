902b59e7c4f91c02e1e2b763322874c1c58172f8
Kuba777ez <kubaa.gil@o2.pl>
2025-04-07 09:35:46 +0200

    wip: function get_user_info

--- main.py
+++ main.py
@@ -4,5 +4,9 @@ users:list[dict] = [
     {'name': 'Marcin', 'location': 'Poznań', 'posts': 420, }
 ]
 
-for user in users:
-    print(f'Twój znajomy: {user["name"]} z {user["location"]} opublikował {user["posts"]}')
+
+def get_user_info(users_data: list[dict])->None:
+    for user in users_data:
+        print(f'Twój znajomy: {user["name"]} z {user["location"]} opublikował {user["posts"]}')
+
+get_user_info(users)
